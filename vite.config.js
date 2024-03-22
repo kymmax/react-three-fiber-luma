d3f131ddbfe1e275fd4a66194c107f08767ee1fa
Jason <>
2024-03-22 17:53:47 +0800

    Update MD and config

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,17 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(({ command, mode }) => {
+
+  return {
+    plugins: [react()],
+    server: {
+      open: true, 
+    },
+    base: mode === "development" ? "" : "/react-three-fiber-luma/",
+    build: {
+      outDir: 'docs'
+    },
+    envDir:"env",
+  }
 })
